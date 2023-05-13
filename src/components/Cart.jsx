c794f3018531867df62e34d28656e7ca057dfec1
shrey kumar singh <shreysingh152004@gmail.com>
2023-05-13 10:07:58 +0530

    "added thisincrease quantity"

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -12,11 +12,18 @@ class Cart extends React.Component{
             qty: 1,
             img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80'
         }
+
+       
+    }
+
+    IncreaseQuantity = () => {
+        console.log('this',this.state)
     }
 
     render(){
 
         const {price,title,qty} = this.state;
+        
 
         return(
             <div className="cart-item">
@@ -33,7 +40,11 @@ class Cart extends React.Component{
                     <div style={{color: '#777'}}>QTY: {qty}</div>
                     <div className="cart-item-actions">
 
-                        <img src="https://cdn-icons-png.flaticon.com/128/992/992651.png" alt="increase"className="action-icons" />
+                        <img src="https://cdn-icons-png.flaticon.com/128/992/992651.png" 
+                             alt="increase"
+                            className="action-icons" 
+                            onClick={this.IncreaseQuantity}
+                        />
                         <img src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" alt="decrease"className="action-icons" />
                         <img src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png" alt="delete"className="action-icons" />
 
